8bcbf4c269c870ef8b3e3ea8a4b4434df42ad8f6
Fanny Cheung <fanny@ynote.hk>
2020-04-07 20:18:53 +0200

    RegExp

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const bangbang = (sentence) => `${sentence}!`
+const bangbang = (sentence, replaceDots) => {
+  if (replaceDots) {
+    return sentence.replace(/\./g, '!')
+  }
+
+  return `${sentence}!`
+}
+
 
 module.exports = bangbang
